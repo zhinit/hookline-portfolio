fa0bb72c3c168e074f3b80ff4129133b4b7d8e37
zachar-AI <zachskihill@gmail.com>
2026-02-20 23:46:09 -0500

    loading songs in script now as opposed to html. getting ready to use web audio api

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,57 @@
+var _a;
+var songs = [
+    {
+        url: "assets/area codes.mp3",
+        trackTitle: "Area Codes",
+    },
+    {
+        url: "assets/bright lights.mp3",
+        trackTitle: "Bright Lights",
+    },
+    {
+        url: "assets/Cinderella new mix.mp3",
+        trackTitle: "Cinderella",
+    },
+    {
+        url: "assets/grillz.mp3",
+        trackTitle: "Grillz",
+    },
+    {
+        url: "assets/helicopta.mp3",
+        trackTitle: "Helicopta",
+    },
+    {
+        url: "assets/i do too.mp3",
+        trackTitle: "I Do Too",
+    },
+    {
+        url: "assets/jjc2.mp3",
+        trackTitle: "Juicy Jay Can't",
+    },
+    {
+        url: "assets/orinocco flow.mp3",
+        trackTitle: "Orinocco Flow",
+    },
+    {
+        url: "assets/praise da lawd.mp3",
+        trackTitle: "Praise The Lawd",
+    },
+];
+for (var _i = 0, songs_1 = songs; _i < songs_1.length; _i++) {
+    var song = songs_1[_i];
+    var audioElement = document.createElement("audio");
+    audioElement.className = "audio-player";
+    audioElement.src = song.url;
+    audioElement.controls = true;
+    (_a = document.querySelector("#audio-container")) === null || _a === void 0 ? void 0 : _a.appendChild(audioElement);
+}
 var audioPlayers = document.querySelectorAll(".audio-player");
 var currPlayer = null;
 var prevPlayer = null;
 audioPlayers.forEach(function (audioPlayer) {
     audioPlayer.addEventListener("play", function (e) {
-        console.log("current player when play is clicked");
-        console.log(currPlayer);
         prevPlayer = currPlayer;
         currPlayer = audioPlayer;
-        console.log("current player after play is clicked");
-        console.log(currPlayer);
-        console.log("previous player after play is clicked");
-        console.log(prevPlayer);
         prevPlayer === null || prevPlayer === void 0 ? void 0 : prevPlayer.pause();
     });
 });
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,3 +1,50 @@
+const songs = [
+  {
+    url: "assets/area codes.mp3",
+    trackTitle: "Area Codes",
+  },
+  {
+    url: "assets/bright lights.mp3",
+    trackTitle: "Bright Lights",
+  },
+  {
+    url: "assets/Cinderella new mix.mp3",
+    trackTitle: "Cinderella",
+  },
+  {
+    url: "assets/grillz.mp3",
+    trackTitle: "Grillz",
+  },
+  {
+    url: "assets/helicopta.mp3",
+    trackTitle: "Helicopta",
+  },
+  {
+    url: "assets/i do too.mp3",
+    trackTitle: "I Do Too",
+  },
+  {
+    url: "assets/jjc2.mp3",
+    trackTitle: "Juicy Jay Can't",
+  },
+  {
+    url: "assets/orinocco flow.mp3",
+    trackTitle: "Orinocco Flow",
+  },
+  {
+    url: "assets/praise da lawd.mp3",
+    trackTitle: "Praise The Lawd",
+  },
+];
+
+for (const song of songs) {
+  const audioElement = document.createElement("audio");
+  audioElement.className = "audio-player";
+  audioElement.src = song.url;
+  audioElement.controls = true;
+  document.querySelector("#audio-container")?.appendChild(audioElement);
+}
+
 const audioPlayers: NodeListOf<HTMLAudioElement> =
   document.querySelectorAll(".audio-player");
 let currPlayer: HTMLAudioElement | null = null;
